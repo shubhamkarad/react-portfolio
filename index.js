9d71e97fe12167c82d731e75cf390023686d7921
Shubham Karad <shubhamkaradsdk07@gmail.com>
2022-08-28 16:21:50 +0530

    'lastes'

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
 const port = process.env.PORT || 4000;
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
 app.use(express.json());
 require("dotenv").config();
 
@@ -15,25 +9,20 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 app.post("/contact-me", (req, res) => {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested, Content-Type, Accept Authorization"
-  // );
   var name = req.body.name;
   var email = req.body.email;
   var message = req.body.message;
   var transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "shubhamdhkarad@gmail.com",
-      pass: "kssffubvwkwvkypa",
+      user: process.env.USER_ID,
+      pass: process.env.PASS_ID,
     },
   });
 
   var mailOptions = {
-    from: "shubhamdhkarad@gmail.com",
-    to: "shubhkrd@gmail.com",
+    from: process.env.USER_ID,
+    to: process.env.SEND_TO_ID,
     subject: "Discussion with Shubham",
     text: "That was easy!",
     html: `<h1>Hey Shubham!</h1><p>${name} needs to contact with you! Please find the below message from ${name}</p>
@@ -41,8 +30,6 @@ app.post("/contact-me", (req, res) => {
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
     if (error) {
       res.send({ status: false, errorMessage: "Something went wrong :(" });
       console.log(error);
